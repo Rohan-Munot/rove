f209da4d661c39f91a353503d82c582de0e9a32b
Rohan Munot <rohanmunot24@gmail.com>
2025-08-23 01:41:00 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-primary text-2xl flex justify-center items-center h-screen">
+    <div className="text-primary text-9xl flex justify-center font-bold items-center h-screen">
       hi
     </div>
   );
